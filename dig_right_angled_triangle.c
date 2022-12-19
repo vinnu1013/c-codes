044bc5f5b5a60c55a57cb67b4268751c04e322bb
vinuu1013 <121045209+vinuu1013@users.noreply.github.com>
2022-12-19 21:16:42 +0530

    Create dig_right_angled_triangle.c

---
+++ dig_right_angled_triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    scanf("%d",&n);        
+    for(i=1;i<=n;i++)      
+     {                       
+        for(int j=1;j<=i;j++){      
+        printf("%d ",j);            
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
